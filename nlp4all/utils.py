b33573444ff229ecdc9036b597a6dda1db6ca33b
Arthur Hjorth <arthur.hjorth@u.northwestern.edu>
2019-05-07 09:34:29 +0200

    Create utils.py

---
+++ nlp4all/utils.py
@@ -0,0 +1,11 @@
+import re
+
+# this function takes a dictionary containing 
+def add_css_class(words, text):
+    categories = list(words.keys())
+    all_words = list(words[categories[0]].keys())
+    text_words = text.split()
+    for w in text_words:
+        css_class = (category, )
+        print(t, )
+
