7b7922c80fbdcb6cdaa5751c71ddee0b74b36ac8
Arthur Hjorth <arthur.hjorth@u.northwestern.edu>
2019-05-08 11:10:18 +0200

    started adding css classes etc

--- dbinit.py
+++ dbinit.py
@@ -6,6 +6,7 @@ import json, os
 from datetime import datetime
 import time
 import json
+import nlp4all.utils
 from sqlalchemy.orm.attributes import flag_modified
 
 db.drop_all()
@@ -14,13 +15,17 @@ db.drop_all()
 db.create_all()
 db.session.commit()
 
-admin_role = Role(name='Admin')
-db.session.add(admin_role)
-student_role = Role(name='Student')
-db.session.add(admin_role)
-teacher_role = Role(name='Teacher')
-db.session.add(admin_role)
-db.session.commit()
+nlp4all.utils.add_role('Admin')
+admin_role = nlp4all.utils.get_role('Admin')
+
+nlp4all.utils.add_role('Student')
+student_role = nlp4all.utils.get_role('Student')
+
+nlp4all.utils.add_role('Teacher')
+teacher_role = nlp4all.utils.get_role('Teacher')
+
+
+
 
 hp = bcrypt.generate_password_hash("Hermes_2014")
 user = User(username="arthurhjorth", email="arthur.hjorth@stx.oxon.org", password=hp)
@@ -40,15 +45,6 @@ db.session.add(user)
 db.session.commit()
 
 
-def clean_word(aword):
-    if "@" in  aword:
-        return "@twitter_ID"
-    if "#" in aword:
-        return "#hashtag"
-    if "http" in aword:
-        return "http://link"
-    return aword
-
 data_dir = 'tweet_data/'
 files = [f for f in os.listdir(data_dir) if '_out.json' in f]
 
@@ -56,74 +52,30 @@ existing_tag_names = []
 for f in files:
     with open(data_dir+f) as inf:
         counter = 0
-        for line in inf.readlines()[:800]:
+        for line in inf.readlines()[:80]:
             indict = json.loads(line)
-#             add cateogyr if it does not already exist
-            if indict['twitter_handle'] not in existing_tag_names:
-                category = TweetTagCategory.query.filter_by(name = indict['twitter_handle']).first()
-                if not category:
-                    category = TweetTagCategory(name = indict['twitter_handle'], description = "Tweet from " + indict['twitter_handle'])
-                    db.session.add(category)
-                    db.session.commit()
-                existing_tag_names.append(indict['twitter_handle'])
             category = TweetTagCategory.query.filter_by(name = indict['twitter_handle']).first()
-            date_str = indict['time']
-            date_rep = '%a %b %d %H:%M:%S %z %Y'
-            unix_time = time.mktime(datetime.strptime(date_str, date_rep).timetuple())
-            timestamp = datetime.fromtimestamp(unix_time)
-            full_text = indict['full_text']
-            links = [w for w in full_text.split() if "http" in w],
-            hashtags = [w for w in full_text.split() if "#" in w],
-            mentions = [w for w in full_text.split() if "@" in w],
-            tweet_parts = [clean_word(w) for w in full_text.split()]
-            full_text=" ".join([w for w in tweet_parts])
-            t = indict['full_text']
-            t = [w for w in t.lower().split() if "#" not in w and "http" not in w and "@" not in w]
-            t = " ".join([w for w in t])
-            t = t.replace(".", " ")
-            t = t.replace("!", " ")
-            t = t.replace("”", " ")
-            t = t.replace("\"", " ")
-            t = t.replace("\'", " ")
-            t = t.replace("“", " ")
-            t = t.replace("?", " ")
-            t = t.replace(":", " ")
-            t = t.replace("/", " ")
-            t = t.replace("-", " ")
-            t = t.replace("–", " ")
-            t = t.replace(",", " ")
-            t = t.replace("\(", " ")
-            t = t.replace("\)", " ")
-            t = t.lower()
-            words = t.split()
-            a_tweet = Tweet(
-                time_posted = timestamp,
-                category = category.id,
-                handle = indict['twitter_handle'],
-                full_text= full_text,
-                words = words,
-                links = links,
-                hashtags = hashtags,
-                mentions = mentions,
-                url = "https://twitter.com/"+indict['twitter_handle']+"/"+str(indict['id']),
-                text = " ".join([clean_word(word) for word in t.split()])
-                )
-            
-            db.session.add(a_tweet)
+            if not category:
+                nlp4all.utils.add_category(indict['twitter_handle'], "Tweet from " + indict['twitter_handle'])
+            existing_tag_names.append(indict['twitter_handle'])
+            category = TweetTagCategory.query.filter_by(name = indict['twitter_handle']).first()
+            nlp4all.utils.add_tweet_from_dict(indict, category)
 
 
-db.session.commit()
-db.session.close()
+
 
 org = Organization.query.first()
 all_cats = TweetTagCategory.query.all()
 cats = [all_cats[0], all_cats[2]]
-print([c.name for c in cats])
-project = Project(name="DF og Ehl", organization=org.id, categories=cats)
 
-db.session.add(project)
-db.session.commit()
+
+
+nlp4all.utils.add_project("DF og EL", org.id, cats)
+
+
+
 analysis = BayesianAnalysis(user = 2, name="Test Analysis", filters=json.dumps([]), features=json.dumps([]),project=1, data = {"counts" : 0, "words" : {}})
+
 
 db.session.add(analysis)
 db.session.commit()
