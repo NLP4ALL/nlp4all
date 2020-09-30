b4258ab603c3df4305cb829c2dddb806735481dc
telmaau <telmis27@live.com>
2020-09-30 10:50:26 +0200

    stopwords added

--- nlp4all/utils.py
+++ nlp4all/utils.py
@@ -264,9 +264,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 
+stopwords = ['og','i','jeg','det','at','en','den','til','er','som','på','de','med','han','af','for','ikke','der','var','mig','sig','men','et','har','om','vi','min','havde','ham','hun','nu','over','da','fra','du','ud','sin','dem','os','op', 'man','hans','hvor','eller','hvad','skal','selv','her','alle','vil','blev','kunne','ind','når','være','dog','noget','ville','jo','deres','efter','ned','skulle','denne','end','dette','mit','også','under','have','dig','anden','hende','mine','alt','meget','sit','sine','vor','mod','disse','hvis','din','nogle','hos','blive','mange','ad','bliver','hendes','været','thi','jer','sådan','#hashtag','@twitter_ID','http://link']
 # for the results
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-tfidfconverter = TfidfVectorizer(analyzer='word', max_features=2000, min_df=3, max_df=0.7)   # added analyzer
+tfidfconverter = TfidfVectorizer(analyzer='word', max_features=2000, min_df=3, max_df=0.7, stop_words=stopwords)   # added stopwords
 logreg = LogisticRegression()
 
 # make pd df
