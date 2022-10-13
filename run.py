0a9b202140a4d1c6493e553d3f4eaa80a7aa2d84
zeyus <zeyus@zeyus.com>
2022-10-13 21:21:37 +0200

    Fixed linting error because of wildcard import.

--- run.py
+++ run.py
@@ -2,7 +2,8 @@
 nlp4all flask app runner.
 """
 from nlp4all import app
-from nlp4all.routes import *
+# @TODO: this needs refactoring, * import is not needed
+from nlp4all.routes import *  # pylint: disable=unused-wildcard-import, wildcard-import
 
 if __name__ == "__main__":
     app.run(debug=True)
