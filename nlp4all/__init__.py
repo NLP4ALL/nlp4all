9381539420914daed29fb4e7b77eb5b299220519
zeyus <zeyus@zeyus.com>
2022-10-13 12:49:02 +0200

    [#4] __init__.py and forms.py passing

--- nlp4all/__init__.py
+++ nlp4all/__init__.py
@@ -1,10 +1,14 @@
-from nlp4all import routes
+"""
+nlp4all module
+"""
+
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from nlp4all import routes
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
--- nlp4all/forms.py
+++ nlp4all/forms.py
@@ -1,3 +1,6 @@
+"""
+Flask forms.
+"""
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
@@ -10,22 +13,35 @@ from wtforms import (
     TextAreaField,
     SelectMultipleField,
     SelectField,
-    RadioField,
     FormField,
     FloatField,
     HiddenField,
 )
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from nlp4all.models import User, Project
 from wtforms.fields.core import Label
+from nlp4all.models import User, Project
+
 
 
 class IMCRegistrationForm(FlaskForm):
+    """
+    IMC Registration form.
+    """
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     submit = SubmitField("Sign Up")
 
 
 class RegistrationForm(FlaskForm):
+    """
+    Registration form.
+    """
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
@@ -36,35 +52,55 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
+        """
+        Validate username.
+        """
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError("That username is taken. Please choose a different one.")
 
     def validate_email(self, email):
+        """
+        Validate email.
+        """
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError("That email is taken. Please choose a different one.")
 
 
 class TaggingForm(FlaskForm):
-    # choices = RadioField()
+    """
+    Tagging form.
+    """
     choices = SelectField("Hvem har skrevet dette tweet?")
     submit = SubmitField("Submit")
 
 
 class TagButton(FlaskForm):
+    """
+    Tagging form.
+    """
     submit = SubmitField("This doesn't matter apparently")
 
     def set_name(self, new_name):
+        """
+        Set name.
+        """
         self.submit.label = Label("", new_name)
 
 
 class AddTweetCategoryForm(FlaskForm):
+    """
+    Add tweet category form.
+    """
     twitter_handle = StringField("Twitter handle", validators=[DataRequired()])
     submit = SubmitField("Create")
 
 
 class LoginForm(FlaskForm):
+    """
+    Login form.
+    """
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
@@ -72,6 +108,9 @@ class LoginForm(FlaskForm):
 
 
 class AddProjectForm(FlaskForm):
+    """
+    Add project form.
+    """
     title = StringField("Title", validators=[DataRequired()])
     description = StringField("Description", validators=[DataRequired()])
     categories = SelectMultipleField(
@@ -83,12 +122,18 @@ class AddProjectForm(FlaskForm):
     submit = SubmitField("Create Project")
 
     def validate_title(self, title):
+        """
+        Validate title.
+        """
         title = Project.query.filter_by(name=title.data).first()
         if title:
             raise ValidationError("That project name is taken. Please choose a different one.")
 
 
 class AddBayesianAnalysisForm(FlaskForm):
+    """
+    Add Bayesian analysis form.
+    """
     name = StringField("Title of Analysis", validators=[DataRequired()])
     shared = BooleanField("Shared project?")
     shared_model = BooleanField("Shared Underlying Model?")
@@ -98,21 +143,33 @@ class AddBayesianAnalysisForm(FlaskForm):
 
 
 class RunBayesianAnalysisRobot(FlaskForm):
+    """
+    Run Bayesian analysis robot form.
+    """
     run_analysis = SubmitField("Run Model!")
 
 
 class AddBayesianRobotForm(FlaskForm):
+    """
+    Add Bayesian robot form.
+    """
     name = StringField("Title of Robot", validators=[DataRequired()])
     submit = SubmitField("Create New Robot")
 
 
 class CreateMatrixForm(FlaskForm):
+    """
+    Create matrix form.
+    """
     categories = SelectMultipleField("Categories to compare", validators=[DataRequired()])
     ratio = IntegerField("Training tweets proportion (%)", validators=[DataRequired()])
     submit = SubmitField("Create Matrix")
 
 
 class ThresholdForm(FlaskForm):
+    """
+    Threshold form.
+    """
     shuffle = BooleanField("Shuffle tweets")
     threshold = FloatField("Set a threshold between 0 and 1")
     ratio = IntegerField("Change training tweets proportion (%)")
@@ -120,13 +177,19 @@ class ThresholdForm(FlaskForm):
 
 
 class AnalysisForm(FlaskForm):
+    """
+    Analysis form.
+    """
     robort_form = FormField(AddBayesianRobotForm)
     add_category_form = FormField(AddTweetCategoryForm)
 
 
 class AddBayesianRobotFeatureForm(FlaskForm):
+    """
+    Add Bayesian robot feature form.
+    """
     feature = StringField(
-        "Add a feature to your machine learning model here. You can use wildcard (*) to search for more words. You can add wildcard in the middle or at the beginning or end of your word. Only one wildcard per search term."
+        "Add a feature to your machine learning model here. You can use wildcard (*) to search for more words. You can add wildcard in the middle or at the beginning or end of your word. Only one wildcard per search term." # pylint: disable=line-too-long
     )
     reasoning = StringField(
         "Explain here why you think this would be a good search term for classifying these texts."
@@ -135,23 +198,35 @@ class AddBayesianRobotFeatureForm(FlaskForm):
 
 
 class BayesianRobotForms(FlaskForm):
+    """
+    Bayesian robot forms.
+    """
     add_feature_form = FormField(AddBayesianRobotFeatureForm)
     run_analysis_form = FormField(RunBayesianAnalysisRobot)
 
 
 class UpdateAccountForm(FlaskForm):
+    """
+    Update account form.
+    """
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField("Email", validators=[DataRequired(), Email()])
     picture = FileField("Update Profile Picture", validators=[FileAllowed(["jpg", "png"])])
     submit = SubmitField("Update")
 
     def validate_username(self, username):
+        """
+        Validate username.
+        """
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError("That username is taken. Please choose a different one.")
 
     def validate_email(self, email):
+        """
+        Validate email.
+        """
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
@@ -159,27 +234,42 @@ class UpdateAccountForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
+    """
+    Post form.
+    """
     title = StringField("Title", validators=[DataRequired()])
     content = TextAreaField("Content", validators=[DataRequired()])
     submit = SubmitField("Post")
 
 
 class AddOrgForm(FlaskForm):
+    """
+    Add organization form.
+    """
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Add Organization")
 
 
 class RequestResetForm(FlaskForm):
+    """
+    Request reset form.
+    """
     email = StringField("Email", validators=[DataRequired(), Email()])
     submit = SubmitField("Request Password Reset")
 
     def validate_email(self, email):
+        """
+        Validate email.
+        """
         user = User.query.filter_by(email=email.data).first()
         if user is None:
             raise ValidationError("There is no account with that email. You must register first.")
 
 
 class ResetPasswordForm(FlaskForm):
+    """
+    Reset password form.
+    """
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField(
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
@@ -188,6 +278,9 @@ class ResetPasswordForm(FlaskForm):
 
 
 class AnnotationForm(FlaskForm):
+    """
+    Annotation form.
+    """
     text = TextAreaField("selectedtext", validators=[DataRequired()])
     start = IntegerField("start")
     end = IntegerField("end")
@@ -196,5 +289,8 @@ class AnnotationForm(FlaskForm):
 
 
 class SelectCategoryForm(FlaskForm):
+    """
+    Select category form.
+    """
     category = IntegerField("cat_id")
     submit = SubmitField("select category")
