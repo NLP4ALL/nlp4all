380ffd3e4be650156975a6fba7aa649d19dcf242
zeyus <zeyus@zeyus.com>
2022-12-16 11:23:56 +0100

    explicit name AS.

--- nlp4all/controllers/__init__.py
+++ nlp4all/controllers/__init__.py
@@ -1,9 +1,9 @@
 """Controllers for the NLP4ALL application."""
 
 # Add classes to top level of module
-from .user import UserController
-from .admin import AdminController
-from .site import SiteController
-from .project import ProjectController
-from .analyses import AnalysesController
-from .data_source import DataSourceController
+from .user import UserController as UserController
+from .admin import AdminController as AdminController
+from .site import SiteController as SiteController
+from .project import ProjectController as ProjectController
+from .analyses import AnalysesController as AnalysesController
+from .data_source import DataSourceController as DataSourceController
--- nlp4all/models/__init__.py
+++ nlp4all/models/__init__.py
@@ -1,14 +1,15 @@
 """Database and models for NLP4ALL."""
 
-from .bayesian_robot import BayesianRobotModel
-from .bayesian_analysis import BayesianAnalysisModel
-from .confusion_matrix import ConfusionMatrixModel
-from .data_source import DataSourceModel
-from .data import DataModel
-from .organization import OrganizationModel
-from .project import ProjectModel
-from .role import RoleModel
-from .data_annotation import DataAnnotationModel
-from .data_tag import DataTagModel
-from .data_tag_category import DataTagCategoryModel
-from .user import UserModel, load_user
+from .bayesian_robot import BayesianRobotModel as BayesianRobotModel
+from .bayesian_analysis import BayesianAnalysisModel as BayesianAnalysisModel
+from .confusion_matrix import ConfusionMatrixModel as ConfusionMatrixModel
+from .data_source import DataSourceModel as DataSourceModel
+from .data import DataModel as DataModel
+from .organization import OrganizationModel as OrganizationModel
+from .project import ProjectModel as ProjectModel
+from .role import RoleModel as RoleModel
+from .data_annotation import DataAnnotationModel as DataAnnotationModel
+from .data_tag import DataTagModel as DataTagModel
+from .data_tag_category import DataTagCategoryModel as DataTagCategoryModel
+from .user import UserModel as UserModel
+from .user import load_user as load_user
